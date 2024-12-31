55296cd5e47fcf2752a5997890ff3dda22b59413
Mohit Mishra <mohitmishra786687@gmail.com>
2024-12-31 17:41:23 +0530

    updating cli view

--- src/cli.c
+++ src/cli.c
@@ -7,28 +7,37 @@
 #include <stdlib.h>
 
 void display_lsm_tree(LSMTree* lsm) {
-    printf("\n--- MemTable Contents ---\n");
-    for (size_t i = 0; i < lsm->memtable->size; i++) {
-        printf("%s: %s\n", lsm->memtable->keys[i], lsm->memtable->values[i]);
+    printf("\n--- LSM Tree Visualization ---\n");
+    printf("MemTable:\n");
+    if (lsm->memtable->size == 0) {
+        printf("  (Empty)\n");
+    } else {
+        for (size_t i = 0; i < lsm->memtable->size; i++) {
+            printf("  %s: %s\n", lsm->memtable->keys[i], lsm->memtable->values[i]);
+        }
     }
-
-    printf("\n--- SSTables Contents ---\n");
-    for (int i = 0; i < lsm->sstable_count; i++) {
-        FILE* file = fopen(lsm->sstables[i]->fname, "r");
-        if (file) {
-            char line[256];
-            while (fgets(line, sizeof(line), file)) {
-                char* value = strchr(line, '\t');
-                if (value) {
-                    *value = '\0';
-                    value++;
-                    if (value[strlen(value) - 1] == '\n') value[strlen(value) - 1] = '\0'; // Remove newline
-                    printf("%s: %s\n", line, value);
+    printf("SSTables:\n");
+    if (lsm->sstable_count == 0) {
+        printf("  (No SSTables)\n");
+    } else {
+        for (int i = 0; i < lsm->sstable_count; i++) {
+            printf("  Level %d:\n", i);
+            FILE* file = fopen(lsm->sstables[i]->fname, "r");
+            if (file) {
+                char line[256];
+                while (fgets(line, sizeof(line), file)) {
+                    char* value = strchr(line, '\t');
+                    if (value) {
+                        *value = '\0';
+                        value++;
+                        if (value[strlen(value) - 1] == '\n') value[strlen(value) - 1] = '\0'; // Remove newline
+                        printf("    %s: %s\n", line, value);
+                    }
                 }
+                fclose(file);
+            } else {
+                printf("    Failed to open %s\n", lsm->sstables[i]->fname);
             }
-            fclose(file);
-        } else {
-            printf("Failed to open %s\n", lsm->sstables[i]->fname);
         }
     }
 }
@@ -46,7 +55,7 @@ void run_cli(LSMTree* lsm) {
             break;
         } else if (strcmp(command, "insert") == 0) {
             if (scanf("%99s %99s", key, value) != 2) {
-                printf("Error reading key value.\n");
+                printf("Error reading key and value.\n");
             } else {
                 if (!lsm_insert(lsm, key, value)) {
                     printf("Failed to insert.\n");
